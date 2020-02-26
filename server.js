6fce26d429e42bf993d02514a8123a20c31b8344
Asa White <whiteasa94@gmail.com>
2020-02-25 23:24:25 -0800

    Currently adding key authentication

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ let currentUsers = 0;
 let userDB = [];
 let usersOnline = [];
 
+let authenticateKey = (name, key) => {
+  const account = usersOnline.find(acc => acc.user === name);
+  return key === account.key;
+};
+
 io.on("connection", function(socket) {
   currentUsers += 1;
   console.log(`${currentUsers} online now`);
@@ -23,7 +28,6 @@ io.on("connection", function(socket) {
       if (userRegistering == null) {
         regiStasus.username = true;
         userDB.push({ username: info.username, password: info.password });
-        console.log(userDB);
       }
     }
     socket.emit("registerStatus", regiStasus);
@@ -34,15 +38,18 @@ io.on("connection", function(socket) {
       user => user.username.toLowerCase() === info.username.toLowerCase()
     );
     if (userLoggingIn == null) {
-      console.log(userLoggingIn);
       socket.emit("loginStatus", { success: false });
     } else {
-      console.log(userLoggingIn.password === info.password);
       if (userLoggingIn.password === info.password) {
+        usersOnline.push({
+          user: userLoggingIn.username,
+          key: Math.floor(100000 + Math.random() * 900000)
+        });
         socket.emit("loginStatus", {
           success: true,
           user: userLoggingIn.username
         });
+        console.log(usersOnline);
       } else {
         socket.emit("loginStatus", { success: false });
       }
